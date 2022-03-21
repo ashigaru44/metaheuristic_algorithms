484dfbcc39756e223014b75b85d43b12c63bbdea
ashigaru44 <ashigaru44@gmail.com>
2022-03-21 16:42:49 +0100

    add rnd_alg_basics

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	//"math/rand"
 
-	"fmt"
+	// "fmt"
 	"meta-heur/tsp/problem"
-	"os/exec"
+	// "os/exec"
 	//"time"
 )
 
@@ -15,13 +15,14 @@ func main() {
 	//r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	problem_path := "./berlin52.tsp"
 	p1 := problem.InitProblem(problem_path)
-	p1.PrintProblem()
+	// p1.PrintProblem()
 	// pr := problem.GenerateProblem(10)
-	saved_problem_path := p1.SaveProblemToFile()
-	fmt.Println(saved_problem_path)
-	err := exec.Command("python", "./visualize.py", saved_problem_path).Run()
-	if err != nil {
-		panic(err)
-	}
+	// saved_problem_path := p1.SaveProblemToFile()
+	// fmt.Println(saved_problem_path)
+	// err := exec.Command("python", "./visualize.py", saved_problem_path).Run()
+	// if err != nil {
+	// panic(err)
+	// }
 	// pr.PrintProblem()
+	problem.RandomAlgorithm(*p1)
 }
--- problem/algorithms.go
+++ problem/algorithms.go
@@ -1,4 +1,27 @@
 package problem
 
-type RandomAlgorithm struct {
+// import "fmt"
+
+// type RandomAlgorithm struct {
+// }
+
+func RandomAlgorithm(p Problem) *[][2]int {
+	temp_nodes := make([][2]int, len(p.nodes)-1)
+	copy(temp_nodes, p.nodes[1:])
+	// fmt.Println("p.nodes:")
+	// for i := range p.nodes {
+	// 	fmt.Println(p.nodes[i])
+	// }
+	// fmt.Println("Temp_nodes:")
+	// for i := range temp_nodes {
+	// 	fmt.Println(temp_nodes[i])
+	// }
+	// fmt.Println("Result_path:")
+	result_path := make([][2]int, len(p.nodes))
+	result_path[0] = p.nodes[0]
+	// for i := range result_path {
+	// fmt.Println(result_path[i])
+	// }
+
+	return &result_path
 }
